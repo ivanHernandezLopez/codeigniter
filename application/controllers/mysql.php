fc835c00d797f8ef18cf929fa2068a3ccd5383ba
ivanHernandezLopez <van.m285@gmail.com>
2014-04-28 21:19:48 -0500

    Configuracion de base de datos, creacion de un controller "mysql" y Modelo "estados" para crear una consulta y verificar su funcionamiento

---
+++ application/controllers/mysql.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Mysql extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model("estados");
+	}
+
+	public function index()
+	{
+		
+		$estados = $this->estados->get_all();
+		foreach($estados->result() as $q)
+			echo $q->nom_largo."<br/>";
+		//$this->load->view('welcome_message');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
